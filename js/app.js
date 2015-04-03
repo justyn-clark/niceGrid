cba713e832a0260e9a5f5db9519b21b222a27516
justyn-clark <ijustynclark@gmail.com>
2015-04-03 00:21:56 -0700

    Update HTML/CSS/JS

--- js/app.js
+++ js/app.js
@@ -1,19 +1,14 @@
+$(document).ready(function (){
 
-jQuery(document).ready(function($){
-$(window).click(function(){
+$("img").hide();
+
+
+$('.row').click(function(){
+
 
  		$('img').slideToggle();
  		
 
  });
+
 });
-
-
-
-
-
-// $(document).ready(function (){
-
-
-
-// });
